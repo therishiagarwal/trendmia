dfd68e09b3f639f3435476e5a1172a845ad15fc1
RishiAgrwal <ra578021@gmail.com>
2024-03-13 13:58:13 +0530

    fixed sing-up issue

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,24 +5,23 @@ from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from myapp.models import CustomUser
 
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
     password1 = forms.CharField(label="Enter Password", widget=forms.PasswordInput())
     password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput())
 
     class Meta:
         model = CustomUser
-        fields = ['name', 'email', 'password1', 'password2']
+        fields = ['email', 'name', 'password1', 'password2']
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords do not match")
-        return password2
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use")
+        return email
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.name = self.cleaned_data['name']
+        user.username = self.cleaned_data['email']
         if commit:
             user.save()
         return user
